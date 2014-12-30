bb264396083bab156ddc9603576ebac99b546826
mgh14 <chasing314@gmail.com>
2014-12-30 16:50:44 -0700

    Remove 'stop' from cycle action enum
    The 'stop' cycle action would behave much the same as the 'pause'
    functionality, so this commit removes it from the enum.

--- search-live-service/src/main/java/mgh14/search/live/service/messaging/CycleAction.java
+++ search-live-service/src/main/java/mgh14/search/live/service/messaging/CycleAction.java
@@ -1,14 +1,13 @@
 package mgh14.search.live.service.messaging;
 
 /**
- * Actions that can be taken by the application
- * cycle loop
+ * Actions that can be taken by the application's
+ * command executor class
  */
 public enum CycleAction {
   START_SERVICE,
   PAUSE,
   RESUME,
-  STOP,
   NEXT,
   PREVIOUS,
   SAVE,
