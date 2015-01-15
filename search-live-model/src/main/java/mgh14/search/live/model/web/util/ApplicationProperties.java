b674f11c8465d54b0e8bdb088387f4e0788d02d0
mgh14 <chasing314@gmail.com>
2015-01-14 23:08:24 -0700

    Move file utils class to util package (part 2)

--- search-live-model/src/main/java/mgh14/search/live/model/web/util/ApplicationProperties.java
+++ search-live-model/src/main/java/mgh14/search/live/model/web/util/ApplicationProperties.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
 
-import mgh14.search.live.model.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
