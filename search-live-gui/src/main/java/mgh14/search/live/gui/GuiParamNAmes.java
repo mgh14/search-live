d5b5ebfc1443214ceeeda15d58f1c3557b70223a
hessm88 <mhess@familysearch.org>
2015-01-29 00:16:11 -0700

    Add gui param names file

---
+++ search-live-gui/src/main/java/mgh14/search/live/gui/GuiParamNAmes.java
@@ -0,0 +1,20 @@
+package mgh14.search.live.gui;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Class that contains parameter names for the GUI's
+ * key-value pairs (e.g. JButtons).
+ */
+@Component
+public class GuiParamNames {
+
+  // Control flow buttons
+  public static final String START_BUTTON = "start-button";
+  public static final String SAVE_BUTTON = "save-button";
+  public static final String PAUSE_BUTTON = "pause-button";
+  public static final String RESUME_BUTTON = "resume-button";
+  public static final String NEXT_BUTTON = "next-button";
+  public static final String DELETE_RESOURCES_BUTTON =
+    "delete-resources-button";
+}
