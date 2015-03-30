3f04f0c6ef644bb68a63359eb0eab447ee33e7da
mhess <mhess@axis41.com>
2015-03-30 16:48:25 -0600

    Create mac wallpaper setter
    In porting this program over to Mac, we need a class that
    converts wallpaper in the Mac way. This commit includes a
    new class that sets the background on mac computers.

---
+++ search-live-model/src/main/java/mgh14/search/live/model/wallpaper/MacWallpaperSetter.java
@@ -0,0 +1,48 @@
+package mgh14.search.live.model.wallpaper;
+
+import java.io.IOException;
+
+import mgh14.search.live.model.web.util.ImageUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class for setting desktop wallpaper given the path to an image
+ * for the Windows operating system.
+ */
+@Component
+public class MacWallpaperSetter {
+
+    private final Logger Log = LoggerFactory.getLogger(getClass().getSimpleName());
+
+    @Autowired
+    private ImageUtils imageUtils;
+
+    public void setDesktopWallpaper(String path) {
+        if (path == null || path.isEmpty()) {
+            Log.error("Invalid null/empty file name");
+            return;
+        }
+        if (!imageUtils.isBitmap(path)) {
+            path = imageUtils.convertImageToBitmap(path);
+        }
+
+        Log.info("Setting resource image to background: [{}]...", path);
+        final String commandArgs[] = {
+                "osascript",
+                "-e", "tell application \"Finder\"",
+                "-e", "set desktop picture to POSIX file \"" + path + "\"",
+                "-e", "end tell"
+        };
+        final Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec(commandArgs);
+        } catch (IOException e) {
+            Log.error("Error executing desktop wallpaper command: ", e);
+        }
+
+    }
+
+}
